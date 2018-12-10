2a235f49a6456a8d74084ab17362576bb2915cc4
Thiagu Nagappan <thiagu.nagappan@rackspace.com>
2018-12-10 15:58:04 -0600

    Update run_config.sh

--- run_config.sh
+++ run_config.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-curl -o /tmp/sdc.tgz -L http://archives.streamsets.com/datacollector/3.6.0/tarball/streamsets-datacollector-all-3.6.0.tgz
+curl -o /tmp/sdc.tgz -L http://archives.streamsets.com/datacollector/3.6.0/tarball/streamsets-datacollector-core-3.6.0.tgz
 
 tar xzf /tmp/sdc.tgz --strip-components 1 -C /opt/local/
